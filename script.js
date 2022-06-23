9b9111c19b2e0496955d465190c18f7700746396
mariahlu17 <62620965+mariahlu17@users.noreply.github.com>
2022-06-24 01:12:32 +0200

    Update script.js
    updating js code

--- script.js
+++ script.js
@@ -8,34 +8,49 @@ const answerSecond = document.querySelector(".answerSecond")
 const answerThird = document.querySelector(".answerThird")
 const answerFourth = document.querySelector(".answerFourth")
 const answerFifth = document.querySelector(".answerFifth")
+const animation = document.querySelector(".animation")
 
+let w = window.outerWidth;
 
+if(w < 1321){
+    animation.style.backgroundImage ="url('./animationTextSmall.png')";
+    animation.style.height = '65px';
+}
+else{
+    animation.style.backgroundImage ="url('./animationText.png')";
+    animation.style.height = '95px';
+}
 
 
 
 function questionFirstShow(){
     questionFirst.classList.toggle('active')
     answerFirst.classList.toggle('active')
+    answerFirst.style.color='#ffffff'
 }
 
 function questionSecondShow(){
     questionSecond.classList.toggle('active')
     answerSecond.classList.toggle('active')
+    answerSecond.style.color='#ffffff'
 }
 
 function questionThirdShow(){
     questionThird.classList.toggle('active')
     answerThird.classList.toggle('active')
+    answerThird.style.color='#ffffff'
 }
 
 function questionFourthShow(){
     questionFourth.classList.toggle('active')
     answerFourth.classList.toggle('active')
+    answerFourth.style.color='#ffffff'
 }
 
 function questionFifthShow(){
     questionFifth.classList.toggle('active')
     answerFifth.classList.toggle('active')
+    answerFifth.style.color='#ffffff'
 }
 
 
